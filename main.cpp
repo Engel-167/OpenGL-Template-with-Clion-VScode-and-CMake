92c7119de5e0256a3c9016261f7e518b2bfaf9f9
Engel Giovany Mendoza Polanco <engelmendoza167@gmail.com>
2025-03-14 11:16:46 -0600

    Example Code changed

--- main.cpp
+++ main.cpp
@@ -1,45 +1,31 @@
-
-#include <iostream>
-#include <glad/glad.h>
 #include <GLFW/glfw3.h>
-int main() {
-  //Inicializamos la libreria GLFW
-  glfwInit();
-  //Le indicamos a GLFW con que version de OpenGL queremos trabajar. En este caso estamos usando OpenGL 3.7
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-  //Le indicamos GLFW que estamos usando solo funciones modernas
-  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  //Establecemos el nombre que deseamos aparezca en pantalla
-  GLFWwindow* window = glfwCreateWindow(800, 800, "Primitivas Graficas",
-  NULL, NULL);
-  //En caso que la ventana no se pueda crear entonces enviamos un error.
-  if (window == NULL) {
-    std::cout << "Fallo al tratar de crear la ventana usando GLFW" <<
-    std::endl;
+int main(void)
+{
+    GLFWwindow* window;
+    if (!glfwInit())
+        return -1;
+
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-0.5f, -0.5f);
+        glVertex2f(0.0f, 0.5f);
+        glVertex2f(0.5f, -0.5f);
+        glEnd();
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
     glfwTerminate();
-    return -1;
-  }
-  //Ponemos la ventana en el contexto actual de lo que queremos presentar en pantalla
-  glfwMakeContextCurrent(window);
-  //Cargamos GLAD para configurar OpenGL
-  gladLoadGL();
-  //Especificamos el puerto de vision de OpenGL en la ventana. En este casotiene las coordenadas desde x = 0, y = 0, hasta x = 800, y = 800
-  glViewport(0, 0, 800, 800);
-  //Especificamos el color de fondo
-  glClearColor(0.07f, 0.13f, 0.17f, 1.0f); //Podemos jugar con los parametrosdel color para obtener diferentes tonos del fondo en la ventana.
-  //Limpiamos el buffer de memoria con el valor que tiene y asignamos unnuevo color si acaso es necesario
-  glClear(GL_COLOR_BUFFER_BIT);
-  //Realizamos un intercambio entre el buffer trasero con el bufferdelantero, es decir, entre el registro viejo de memoria con el nuevo registro dememoria
-  glfwSwapBuffers(window);
-  while (!glfwWindowShouldClose(window))
-  {
-    //Tomamos en consideracion todos los eventos que le puedan ser indicados a GLFW
-    glfwPollEvents();
-  }
-  //Eliminamos la ventana antes de que termine el programa
-  glfwDestroyWindow(window);
-  //Terminamos de ejecutar GLFW antes de que termine el programa
-  glfwTerminate();
-  return 0;
+    return 0;
 }
